50c83676e6deaa73ab3b7a4b3218f9be818ec90a
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-19 16:41:08 -0300

    feat03: leitura da conta na classe Application

--- src/main/java/com/betrybe/bankaccount/Application.java
+++ src/main/java/com/betrybe/bankaccount/Application.java
@@ -1,5 +1,8 @@
 package com.betrybe.bankaccount;
 
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * A class.
  */
@@ -9,7 +12,23 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Informe o número da conta: ");
+    String unformattedAccountNumber = scanner.nextLine();
 
+    // https://commons.apache.org/proper/commons-lang/apidocs/org/apache/commons/lang3/StringUtils.html
+    if (!StringUtils.isNumeric(unformattedAccountNumber)) {
+      System.out.println("Número da conta inválido!");
+      return;
+    }
+
+    AccountNumberFormatter formatter = new AccountNumberFormatter();
+    String accountNumber = formatter.formatAccountNumber(
+        Integer.parseInt(unformattedAccountNumber)
+    );
+
+    System.out.println("Número da conta: " + accountNumber);
+    scanner.close();
   }
 
 }
